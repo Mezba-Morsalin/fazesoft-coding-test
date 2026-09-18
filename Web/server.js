899085bc65712e44d5996768ace04ecbb454b997
Mezba Morsalin <mejbahmorsalin560@hotmail.com>
2026-09-18 23:08:02 +0600

    Task Completed

--- Web/server.js
+++ Web/server.js
@@ -6,12 +6,46 @@ const PORT = 5000
 app.use(express.json());
 
 
-const task = []
+const tasks = [];
 let nextId = 1;
 
 const categories = ["work", "personal", "study"];
 
-app.post("/api/task")
+app.post("/api/task", (req, res)=> {
+    const { title, category} = req.body
+
+    if (typeof title !== "string" || title.trim() === "") {
+        return res.status(400).json({
+            error : "Title is required and cannot accept empty space"
+        })
+    }
+
+    if (typeof category !== "string" || !category.includes((category)) === "") {
+        return res.status(400).json({
+            error : "category must worked, personal, or study"
+        })
+    }
+
+    const task = {
+        id : nextId++,
+        title : title.trim(),
+        category : category,
+        status : "pending"
+    };
+    tasks.push(task);
+    res.status(201).json((task))
+});
+
+app.get("/api/tasks", (req, res) => {
+    const {category} = req.query;
+
+    if(!category) {
+        return res.status(200).json(tasks);
+    }
+    const filterTasks = tasks.filter(task => task.category === category);
+    
+    res.status(200).json(filterTasks);
+})
 
 app.get("/", (req, res) => {
       res.send("Task Running Successfully");
